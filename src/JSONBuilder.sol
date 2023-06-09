b4b2156060d87f8327be5145200aaf0b32dcdf04
Jason Vranek <jvranek@ucsc.edu>
2023-06-09 12:15:54 -0700

    Add cheaper alternative to decoding a JSON

---
+++ src/JSONBuilder.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.8.13;
+
+contract JSONBuilder {
+    struct Values {
+        bytes id;
+        bytes timestamp;
+        bytes version;
+        bytes epidPseudonym;
+        bytes advisoryURL;
+        bytes advisoryIDs;
+        bytes isvEnclaveQuoteStatus;
+        bytes isvEnclaveQuoteBody;
+    }
+
+    function buildJSON(Values memory values) public pure returns (string memory json) {
+        json = string(
+            abi.encodePacked(
+                '{"id":"',
+                values.id,
+                '","timestamp":"',
+                values.timestamp,
+                '","version":',
+                values.version,
+                ',"epidPseudonym":"',
+                values.epidPseudonym
+            )
+        );
+        json = string(
+            abi.encodePacked(
+                json,
+                '","advisoryURL":"',
+                values.advisoryURL,
+                '","advisoryIDs":',
+                values.advisoryIDs,
+                ',"isvEnclaveQuoteStatus":"',
+                values.isvEnclaveQuoteStatus,
+                '","isvEnclaveQuoteBody":"',
+                values.isvEnclaveQuoteBody,
+                '"}'
+            )
+        );
+    }
+}
+
+contract CustomJSONBuilder {
+    string[] public keys;
+
+    constructor(string[] memory _keys) {
+        keys = _keys;
+    }
+
+    function buildJSON(string[] memory values) public view returns (string memory) {
+        require(values.length == keys.length);
+        string memory json = "";
+        for (uint256 i = 0; i < keys.length; i++) {
+            json = string(abi.encodePacked(json, keys[i], values[i]));
+        }
+        return string(abi.encodePacked("{", json, '"}'));
+    }
+}
